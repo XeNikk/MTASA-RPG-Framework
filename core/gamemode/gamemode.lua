550ce1e09d5547043a0f9199b667ec20f881dbc4
XeN <41595195+XeNikk@users.noreply.github.com>
2020-06-23 17:12:25 +0200

    Poprawka

--- core/gamemode/gamemode.lua
+++ core/gamemode/gamemode.lua
@@ -9,12 +9,12 @@ function getGamemodeConfig(data)
 end
 
 function refreshGamemodeConf()
-    local query = exports.db:queryTable("SELECT * FROM `gamemode` LIMIT 1", "gamemode")
-    if query and #query > 0 then
-        gamemode["name"] = query[1].server_name
-        gamemode["money_multiplayer"] = query[1].money_multiplayer
-        gamemode["free_premium"] = query[1].free_premium == 1 and true or false
-        setGameType(query[1].game_type)
+    local results = exports.db:queryTable("SELECT * FROM %s LIMIT 1", "gamemode")
+    if results and #results > 0 then
+        gamemode["name"] = results[1].server_name
+        gamemode["money_multiplayer"] = results[1].money_multiplayer
+        gamemode["free_premium"] = results[1].free_premium == 1 and true or false
+        setGameType(results[1].game_type)
     else
         outputDebugString("["..getResourceName(getThisResource()).."/gamemode/gamemode.lua] Nie udało się załadować konfiguracji serwera z bazy danych!", 1)
     end
